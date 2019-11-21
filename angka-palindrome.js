cbe976451be8a80a7d2c1d7f55d025b8a6bae5e1
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-21 20:21:19 +0700

    Solved

--- angka-palindrome.js
+++ angka-palindrome.js
@@ -6,4 +6,25 @@
 
 
 
+function angkaPalindrome(num) {
+    num++;
+    let numString = num.toString();
+    let numReversed = '';
+    
+    for (let i = numString.length - 1; i >= 0; i--) {
+        numReversed += numString[i];
+    }
 
+    if (numString === numReversed) {
+        return num;
+    } else {
+        return angkaPalindrome(num);
+    }
+}
+
+// TEST CASES
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
