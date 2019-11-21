0bd0ca325272f12d8a589c12d96d7729288f7f70
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-21 19:37:24 +0700

    Solved

---
+++ palindrome.js
@@ -0,0 +1,29 @@
+/**
+ * HACKTIV8 Phase 0 - Menggunakan Switch Case
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+ */
+
+
+
+
+
+function palindrome(kata) {
+    let kataReversed = '';
+
+    for (i = kata.length - 1; i >= 0; i--) {
+        kataReversed += kata[i];
+    }
+
+    if (kata === kataReversed) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
