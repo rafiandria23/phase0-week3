8fd395c6aabda2c23c2b10eb516c1f621c931e7b
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-22 15:19:05 +0700

    Added codeacademy-javascript-arrays.png

---
+++ deret-aritmatika.js
@@ -0,0 +1,16 @@
+/**
+ * HACKTIV8 Phase 0 - Menggunakan Switch Case
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+*/
+
+
+
+
+
+function tentukanDeretAritmatika(arr) {
+    let deviation = arr[1] - arr[0];
+    
+    if (deviation === ) {
+        
+    }
+}
