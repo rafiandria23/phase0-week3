e3a414e364136b43184f2cee7f31079ed8811931
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-22 12:04:03 +0700

    Added TEST CASES

--- pasangan-angka-terbesar.js
+++ pasangan-angka-terbesar.js
@@ -17,4 +17,9 @@ function pasanganTerbesar(num) {
     return max;
 }
 
-console.log(pasanganTerbesar(123456789));
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
