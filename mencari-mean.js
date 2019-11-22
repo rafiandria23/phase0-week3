7b9a2b6f87e47f734caf780a72277a1976728461
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-22 12:29:18 +0700

    Solved

---
+++ mencari-mean.js
@@ -0,0 +1,22 @@
+/**
+ * HACKTIV8 Phase 0 - Menggunakan Switch Case
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+*/
+
+
+
+
+
+function cariMean(arr) {
+    let arrSum = arr.reduce((a, b) => a + b);
+    let arrMean = arrSum / arr.length;
+    let arrResult = Math.round(arrMean);
+    return arrResult;
+}
+
+// TEST CASES
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 2
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
