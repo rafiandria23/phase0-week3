deccba1444507644ff471ef216fd992736a1e8d3
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-26 14:12:43 +0700

    Solved!

---
+++ kelompok-angka.js
@@ -0,0 +1,29 @@
+/**
+ * HACKTIV8 Phase 0 - Logic Challenge - Mengelompokkan Angka
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+*/
+
+
+
+
+
+function mengelompokkanAngka(arr) {
+    let arrResult = [ [], [], [] ];
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] % 2 === 0) && (arr[i] % 3 !== 0)) {
+            arrResult[0].push(arr[i]);
+        } else if ((arr[i] % 2 !== 0) && (arr[i] % 3 !== 0)) {
+            arrResult[1].push(arr[i]);
+        } else if (arr[i] % 3 === 0) {
+            arrResult[2].push(arr[i]);
+        }
+    }
+
+    return arrResult;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
